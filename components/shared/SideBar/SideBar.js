8cc44e003e5abf2b7b792a38153fe9bf45f0f2db
aggyreina <aggyreina@gmail.com>
2021-07-09 13:43:21 +0200

    Create signup form

---
+++ components/shared/SideBar/SideBar.js
@@ -0,0 +1,11 @@
+const SideBar = () => {
+    return(
+        <div>
+            <h3>Register for MPA</h3>
+            <p>To keep connecting with us, please register with your personnel info</p>
+        </div>
+    )
+
+}
+
+export default SideBar;
---
+++ components/shared/Signup.js
@@ -0,0 +1,34 @@
+// import { useForm } from 'react-hook-form';
+// import { Alert } from 'react-bootstrap';
+
+// const PortfolioForm = () => {
+
+//   return (
+//     <form onSubmit={handleSubmit(onSubmit)}>
+//       <div className="form-group">
+//         <label htmlFor="title">Title</label>
+//         <input
+//           name="title"
+//           type="text"
+//           className="form-control"
+//           id="title"/>
+//       </div>
+
+//       <div className="form-group">
+//         <label htmlFor="city">Company</label>
+//         <input
+//           name="company"
+//           type="text"
+//           className="form-control"
+//           id="company"/>
+//       </div>
+
+//       <button
+//         type="submit"
+//         className="btn btn-primary">Create
+//       </button>
+//     </form>
+//   )
+// }
+
+// export default PortfolioForm;
